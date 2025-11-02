486d97ee0c28075e2bf97d47c7b0dbe6f72ee905
Nadyaaurora <nadyaaurora575@gmail.com>
2025-11-02 23:35:44 +0700

    JOBSHEET 8

--- PERTEMUAN 8/upload.php
+++ PERTEMUAN 8/upload.php
@@ -1,20 +1,22 @@
 <?php
-if (isset($_POST["submit"])) {
-    $targetDirectory = "documents/";
-    $targetFile = $targetDirectory . basename($_FILES["documentToUpload"]["name"]);
-    $documentFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+if(isset($_POST["submit"])){
+    $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
     $allowedExtensions = array("txt", "pdf", "doc", "docx");
-    $maxFileSize = 10 * 1024 * 1024;
+    $maxsize = 3*1024*1024;
 
-    if (in_array($documentFileType, $allowedExtensions) && $_FILES["documentToUpload"]["size"] <= $maxFileSize) {
-        if (move_uploaded_file($_FILES["documentToUpload"]["tmp_name"], $targetFile)) {
-            echo "Dokumen berhasil diunggah.";
-        } else {
-            echo "Gagal mengunggah dokumen.";
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize)
+    {
+        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+            echo "File berhasil diunggah";
         }
-    }  else{
-        echo "Jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+        else{
+            echo "Gagal mengunggah file.";
+        }
     }
+}else{
+    echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
 }
 ?>
