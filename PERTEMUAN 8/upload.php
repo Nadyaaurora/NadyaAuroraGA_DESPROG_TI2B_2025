88056cf017292256eae5285a032b48a0d4f3c18b
Nadyaaurora <nadyaaurora575@gmail.com>
2025-11-02 21:31:28 +0700

    upload 1.4

--- PERTEMUAN 8/upload.php
+++ PERTEMUAN 8/upload.php
@@ -1,67 +1,20 @@
 <?php
 if (isset($_POST["submit"])) {
-    $targetDirectory = "uploads/";
-    $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
-    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $targetDirectory = "documents/";
+    $targetFile = $targetDirectory . basename($_FILES["documentToUpload"]["name"]);
+    $documentFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxFileSize = 5 * 1024 * 1024;
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxFileSize = 10 * 1024 * 1024;
 
-    if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-            echo "File berhasil diunggah.<br>";
-
-            // LANGKAH 5: Membuat thumbnail
-            $thumbnailWidth = 200;
-            list($width, $height) = getimagesize($targetFile);
-            $ratio = $width / $thumbnailWidth;
-            $thumbnailHeight = $thumbnailWidth * $ratio;
-
-            // Buat resource gambar asli
-            switch ($fileType) {
-                case "jpg":
-                case "jpeg":
-                    $srcImage = imagecreatefromjpeg($targetFile);
-                    break;
-                case "png":
-                    $srcImage = imagecreatefrompng($targetFile);
-                    break;
-                case "gif":
-                    $srcImage = imagecreatefromgif($targetFile);
-                    break;
-            }
-
-            // Buat resource untuk thumbnail
-            $thumbImage = imagecreatetruecolor($thumbnailWidth, $thumbnailHeight);
-
-            // Resize gambar asli ke thumbnail
-            imagecopyresampled($thumbImage, $srcImage, 0, 0, 0, 0, $thumbnailWidth, $thumbnailHeight, $width, $height);
-
-            // Simpan thumbnail ke folder uploads
-            $thumbnailFile = $targetDirectory . "thumb_" . basename($_FILES["fileToUpload"]["name"]);
-
-            switch ($fileType) {
-                case "jpg":
-                case "jpeg":
-                    imagejpeg($thumbImage, $thumbnailFile);
-                    break;
-                case "png":
-                    imagepng($thumbImage, $thumbnailFile);
-                    break;
-                case "gif":
-                    imagegif($thumbImage, $thumbnailFile);
-                    break;
-            }
-
-            imagedestroy($srcImage);
-            imagedestroy($thumbImage);
-
-            echo "Thumbnail berhasil dibuat: <a href='{$thumbnailFile}'>{$thumbnailFile}</a>";
+    if (in_array($documentFileType, $allowedExtensions) && $_FILES["documentToUpload"]["size"] <= $maxFileSize) {
+        if (move_uploaded_file($_FILES["documentToUpload"]["tmp_name"], $targetFile)) {
+            echo "Dokumen berhasil diunggah.";
         } else {
-            echo "Gagal mengunggah file.";
+            echo "Gagal mengunggah dokumen.";
         }
-    } else {
-        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+    }  else{
+        echo "Jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
 }
 ?>
