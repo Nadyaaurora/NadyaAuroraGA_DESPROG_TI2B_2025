16de02fd66559972559b115b1dce0899579bd96f
Nadyaaurora <nadyaaurora575@gmail.com>
2025-11-02 21:18:43 +0700

    soal no 1.3

--- PERTEMUAN 8/upload.php
+++ PERTEMUAN 8/upload.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST["submit"])) {
-    $targetDirectory = "uploads/"; // Direktori tujuan untuk menyimpan file
+    $targetDirectory = "uploads/";
     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
     $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
@@ -9,7 +9,54 @@ if (isset($_POST["submit"])) {
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-            echo "File berhasil diunggah.";
+            echo "File berhasil diunggah.<br>";
+
+            // LANGKAH 5: Membuat thumbnail
+            $thumbnailWidth = 200;
+            list($width, $height) = getimagesize($targetFile);
+            $ratio = $width / $thumbnailWidth;
+            $thumbnailHeight = $thumbnailWidth * $ratio;
+
+            // Buat resource gambar asli
+            switch ($fileType) {
+                case "jpg":
+                case "jpeg":
+                    $srcImage = imagecreatefromjpeg($targetFile);
+                    break;
+                case "png":
+                    $srcImage = imagecreatefrompng($targetFile);
+                    break;
+                case "gif":
+                    $srcImage = imagecreatefromgif($targetFile);
+                    break;
+            }
+
+            // Buat resource untuk thumbnail
+            $thumbImage = imagecreatetruecolor($thumbnailWidth, $thumbnailHeight);
+
+            // Resize gambar asli ke thumbnail
+            imagecopyresampled($thumbImage, $srcImage, 0, 0, 0, 0, $thumbnailWidth, $thumbnailHeight, $width, $height);
+
+            // Simpan thumbnail ke folder uploads
+            $thumbnailFile = $targetDirectory . "thumb_" . basename($_FILES["fileToUpload"]["name"]);
+
+            switch ($fileType) {
+                case "jpg":
+                case "jpeg":
+                    imagejpeg($thumbImage, $thumbnailFile);
+                    break;
+                case "png":
+                    imagepng($thumbImage, $thumbnailFile);
+                    break;
+                case "gif":
+                    imagegif($thumbImage, $thumbnailFile);
+                    break;
+            }
+
+            imagedestroy($srcImage);
+            imagedestroy($thumbImage);
+
+            echo "Thumbnail berhasil dibuat: <a href='{$thumbnailFile}'>{$thumbnailFile}</a>";
         } else {
             echo "Gagal mengunggah file.";
         }
